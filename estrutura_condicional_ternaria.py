bac57c83c6f4f7e006e9f6b0d9695dc37a05a99a
Wellington Henrique França de Santana <53205033+WellHFSantana@users.noreply.github.com>
2024-03-19 20:19:32 -0300

    Python Data Analytics
    Arquivos de exemplos do curso Python Data Analytics da plataforma DIO

---
+++ estrutura_condicional_ternaria.py
@@ -0,0 +1,5 @@
+saldo  = 2000
+saque = 2500
+
+status =  "sucesso" if "Saldo relaizar meu filho."
+print(f{}
---
+++ variaveis_constantes.py
@@ -0,0 +1,11 @@
+nome = "Wellington"
+idade = "34"
+nome = "Taciane"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC"]
+
+print(BRAZILIAN_STATES) 
